5598aede9c9c25a59a1053334bd6dd0df2c72818
augustovictor <victoraweb@gmail.com>
2019-05-12 20:28:53 -0300

    [TEST] Test with pact

--- src/main/kotlin/com/github/augustovictor/springpactconsumerdrivencontract/jsonplaceholder/JsonPlaceholderClient.kt
+++ src/main/kotlin/com/github/augustovictor/springpactconsumerdrivencontract/jsonplaceholder/JsonPlaceholderClient.kt
@@ -4,8 +4,8 @@ import com.github.augustovictor.springpactconsumerdrivencontract.user.User
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 
-@FeignClient(name = "JsonPlaceholder", url = "https://jsonplaceholder.typicode.com")
+@FeignClient(name = "JsonPlaceholder", url = "http://localhost:8092/users")
 interface JsonPlaceholderClient {
-    @GetMapping(value = ["/users"])
+    @GetMapping
     fun fetchUsers(): List<User>
 }
