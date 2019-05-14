0c4d76f498fe68d24b2d117571fff0e632218d5d
augustovictor <victoraweb@gmail.com>
2019-05-13 21:21:11 -0300

    [FEAT] Add external service call

--- src/main/kotlin/com/github/augustovictor/springpactconsumerdrivencontract/jsonplaceholder/JsonPlaceholderClient.kt
+++ src/main/kotlin/com/github/augustovictor/springpactconsumerdrivencontract/jsonplaceholder/JsonPlaceholderClient.kt
@@ -4,8 +4,8 @@ import com.github.augustovictor.springpactconsumerdrivencontract.user.User
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 
-@FeignClient(name = "JsonPlaceholder", url = "http://localhost:8092/users")
+@FeignClient(name = "JsonPlaceholder", url = "http://localhost:3000")
 interface JsonPlaceholderClient {
-    @GetMapping
+    @GetMapping(path = ["/external/users"])
     fun fetchUsers(): List<User>
 }
